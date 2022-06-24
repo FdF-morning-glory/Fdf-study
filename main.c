dc35ad9ea009a9b7d53e40d60881cc2a9e4f01de
nong508 <86358498+nong508@users.noreply.github.com>
2022-06-24 15:14:35 +0900

    init

---
+++ main.c
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinypark <jinypark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 16:06:19 by jinypark          #+#    #+#             */
+/*   Updated: 2022/06/21 18:43:42 by jinypark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx_macos/mlx.h"
+#include "./gnl/get_next_line.h"
+#include "./libft/libft.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <math.h>
+#define abs(a) a >= 0 ? a : a * -1
+
+// static void	rotate_x(int *y, int *z, double alpha)
+// {
+// 	int	prev_y;
+
+// 	prev_y = *y;
+// 	*y = prev_y * cos(alpha) + (*z) * sin(alpha);
+// 	*z = -prev_y * sin(alpha) + (*z) * cos(alpha);
+// }
+
+// static void	rotate_y(int *x, int *z, double beta)
+// {
+// 	int	prev_x;
+
+// 	prev_x = *x;
+// 	*x = prev_x * cos(beta) + (*z) * sin(beta);
+// 	*z = -prev_x * sin(beta) + (*z) * cos(beta);
+// }
+
+// static void	rotate_z(int *x, int *y, double gamma)
+// {
+// 	int	prev_x;
+// 	int	prev_y;
+
+// 	prev_x = *x;
+// 	prev_y = *y;
+// 	*x = prev_x * cos(gamma) - prev_y * sin(gamma);
+// 	*y = prev_x * sin(gamma) + prev_y * cos(gamma);
+// }
+
+
+void	dda(float x1, float y1, float x2, float y2, void *mlx, void *window, int n)
+{
+	float dx, dy, xinc, yinc, step, i = 0.0;
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+    // dx는 x좌표의 차이, dy는 y좌표의 차이
+
+    if (abs(dx) > abs(dy))
+    	step = abs(dx);
+    else
+    	step = abs(dy);
+    // dx > dy : 기울기가 1보다 작을경우
+    // dx < dy : 기울기가 1보다 클 경우
+
+    xinc = dx / step;
+    yinc = dy / step;
+    while (i <= step)
+    {
+		if (n == 1)
+    		mlx_pixel_put(mlx, window, x1, y1, 0xFF0000);
+		else
+			mlx_pixel_put(mlx, window, x1, y1, 0xFFFFFF);
+        x1 = x1 + xinc;
+        y1 = y1 + yinc;
+		i += 1.0;
+    }
+}
+
+static void	ft_isometric(int *x, int *y, int z)
+{
+	int	prev_x;
+	int	prev_y;
+
+	prev_x = *x;
+	prev_y = *y;
+	*x = (prev_x - prev_y) * cos(M_PI/6);
+	*y = (prev_x + prev_y) * cos(M_PI/3) - z;
+}
+
+void print_pixel(int x, int y, int z, void *mlx, void *win)
+{
+	ft_isometric(&x, &y, z);
+	// rotate_x(&y, &z, 35.26);
+	// rotate_z(&x, &y, 45);
+	mlx_pixel_put(mlx, win, x + 200 , y + 200, 0xFFFF00);
+}
+
+int main()
+{
+	void	*mlx;
+	void	*window;
+	// char	*line;
+	// int		x;
+	// char	**split;
+
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, 700, 1500, "FdF");
+
+	// int arr[100][100] = {0, };
+	// arr[0][0] = 0;
+	// arr[0][300] = 0;
+	// arr[300][300] = 0;
+	// arr[30][30] = -30;
+	// arr[30][0] = 0;
+	// arr[30][0] = -30;
+	// arr[0][0] = -30;
+
+	// (void)rotate_x;
+	// (void)rotate_y;
+	print_pixel(100,0,0,mlx, window);
+	print_pixel(0,-100,0,mlx, window);
+	print_pixel(100,-100,0,mlx, window);
+	print_pixel(100,0,100,mlx, window);
+	print_pixel(100,-100,100,mlx, window);
+	print_pixel(0,0,100,mlx, window);
+	print_pixel(0,-100,100,mlx, window);
+	print_pixel(0,0,0,mlx, window);
+
+
+
+
+
+	// for (int x = 0; x < 180; x += 20)
+	// {
+	// 	for (int y = 0; y < 200; y += 20)
+	// 	{
+	// 		if (y == 0 && x ==0)
+	// 			mlx_pixel_put(mlx,window, 100, 100, 0xFFFFFF);
+	// 		else if (ft[y/20][x/20] == 1)
+	// 			dda(100+x, 100+(y - 20) , 100+x, 100+y, mlx, window, 1);
+	// 		else if (ft[y/20][x/20] == 0 && y != 0)
+	// 			dda(100+x, 100+(y - 20) , 100+x, 100+y, mlx, window, 0);
+	// 	}
+	// }
+	mlx_loop(mlx);
+	return (0);
+}
+
+
+// [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,1,1,0,0,1,1,0],[0,0,1,1,0,0,1,1,0],[0,0,1,1,0,0,1,1,0],[0,0,1,1,1,1,1,1,0],[0,0,0,1,1,1,1,1,0],[0,0,0,0,0,0,1,1,0]
