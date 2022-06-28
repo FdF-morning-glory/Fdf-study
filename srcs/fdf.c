2cd2d0ce7fb8358f75824a54c5722b694d8280bb
Elineely <sooleeny@naver.com>
2022-06-28 09:40:09 +0900

    create srcs folder

---
+++ srcs/fdf.c
@@ -0,0 +1,20 @@
+#include "minilibx_macos/mlx.h"
+#include "./gnl/get_next_line.h"
+#include "./libft/libft.h"
+#include <fcntl.h>
+#include <math.h>
+#include <stdio.h>
+
+void	fdf()
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	//예외처리 (직사각형이 아닌 경우)
+	//파일열고()
+	//파싱받고()
+	//파일 닫고(fd 누수)
+	
+}
